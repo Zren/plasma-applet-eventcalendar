fe43af08e0baa65164ef37caf53cf75f439c1ef9
Chris Holland <zrenfire@gmail.com>
2017-04-29 23:20:19 -0400

    Translate the event durations

--- package/contents/ui/shared.js
+++ package/contents/ui/shared.js
@@ -114,11 +114,20 @@ function formatEventTime(dateTime, args) {
     return Qt.formatDateTime(dateTime, timeFormat)
 }
 
+function formatEventDateTime(dateTime, args) {
+    var shortDateFormat = i18nc("short month+date format", "MMM d");
+    var dateStr = Qt.formatDateTime(dateTime, shortDateFormat);
+    var timeStr = formatEventTime(dateTime, args);
+    return i18nc("date (%1) with time (%2)", "%1, %2", dateStr, timeStr);
+}
+
 function formatEventDuration(event, args) {
     var relativeDate = args && args.relativeDate;
     var clock_24h = args && args.clock_24h;
     var startTime = event.start.dateTime;
     var endTime = event.end.dateTime;
+    var shortDateFormat = i18nc("short month+date format", "MMM d");
+
     if (event.start.date) {
         // GCal ends all day events at midnight, which is technically the next day.
         // Humans consider the event to end at 23:59 the day before though.
@@ -127,24 +136,29 @@ function formatEventDuration(event, args) {
         if (isSameDate(startTime, dayBefore)) {
             return i18n("All Day");
         } else {
-            var s = Qt.formatDateTime(startTime, "MMM d");
-            s += " - ";
-            s += Qt.formatDateTime(dayBefore, "MMM d");
-            return s;
+            var startStr = Qt.formatDateTime(startTime, shortDateFormat);
+            var endStr = Qt.formatDateTime(dayBefore, shortDateFormat);
+            return i18nc("from date/time %1 until date/time %2", "%1 - %2", startStr, endStr);
         }
     } else {
-        var s = "";
+        var startStr;
         if (!relativeDate || isDateEarlier(startTime, relativeDate)) {
-            s += Qt.formatDateTime(startTime, "MMM d") + ", ";
+            startStr = formatEventDateTime(startTime, args); // MMM d, h:mm AP
+        } else {
+            startStr = formatEventTime(startTime, args); // h:mm AP
         }
-        s += formatEventTime(startTime, { clock_24h: clock_24h });
-        if (startTime.valueOf() != endTime.valueOf()) {
-            s += " - ";
-            if (!isSameDate(startTime, endTime)) {
-                s += Qt.formatDateTime(endTime, "MMM d") + ", ";
-            }
-            s += formatEventTime(endTime, { clock_24h: clock_24h });
+
+        if (startTime.valueOf() == endTime.valueOf()) {
+            return startStr; // Don't need the end time
         }
-        return s;
+
+        var endStr;
+        if (isSameDate(startTime, endTime)) {
+            endStr = formatEventTime(endTime, args); // MMM d, h:mm AP - h:mm AP
+        } else {
+            // !isSameDate, so we need to add the date
+            endStr = formatEventDateTime(endTime, args); // MMM d, h:mm AP - MMM d, h:mm AP
+        }
+        return i18nc("from date/time %1 until date/time %2", "%1 - %2", startStr, endStr);
     }
 }
