Zren/plasma-applet-eventcalendar

commit 3646cf5caad64761b527ba51f9fa222c35c1cf51

Add link to agenda weather. Remove weather text, increase icon to 24px. Prep weather tooltip. Fix agenda debug event showing up. Add agenda weather debug data. Use Qt date if when debugging clock (instead of datasource). Properly scale FontIcon to height. Prep 2 line clock Update popup height on expand.

package/contents/ui/config/ConfigGeneral.qml
@@ -18,6 +18,8 @@ Item {
     property alias cfg_clock_24h: clock_24h.checked
     property alias cfg_clock_show_seconds: clock_show_seconds.checked
     property alias cfg_clock_timeformat: clock_timeformat.text
+    property alias cfg_clock_timeformat_2: clock_timeformat_2.text
+    property alias cfg_clock_line_2: clock_line_2.checked
     property alias cfg_clock_mousewheel_up: clock_mousewheel_up.text
     property alias cfg_clock_mousewheel_down: clock_mousewheel_down.text
     property alias cfg_timer_repeats: timer_repeats.checked
@@ -34,6 +36,26 @@ Item {
     }
 
     Layout.fillWidth: true
+
+
+
+    function onClockFormatChange() {
+        var combinedFormat = cfg_clock_timeformat;
+        if (cfg_clock_line_2) {
+            combinedFormat += '\n' + cfg_clock_timeformat_2;
+        }
+        var is12hour = combinedFormat.toLowerCase().indexOf('ap') >= 0;
+        cfg_clock_24h = !is12hour;
+        cfg_clock_show_seconds = combinedFormat.indexOf('s') >= 0
+    }
+
+    // Component.onCompleted: {
+    //     cfg_clock_timeformat = 'h:mm ap'
+    //     cfg_clock_timeformat_2 = 'MMM d, yyyy'
+    //     cfg_clock_line_2 = true
+    // }
+
+
 
     ColumnLayout {
         id: pageColumn
@@ -112,24 +134,63 @@ Item {
                     cursorShape: parent.hoveredLink ? Qt.PointingHandCursor : Qt.ArrowCursor
                 }
             }
+
             RowLayout {
                 Layout.fillWidth: true
+                CheckBox {
+                    enabled: false
+                    checked: true
+                }
                 Label {
                     text: i18n("Line 1:")
                 }
                 TextField {
                     Layout.fillWidth: true
                     id: clock_timeformat
-
-                    onTextChanged: {
-                        var is12hour = text.toLowerCase().indexOf('ap') >= 0;
-                        cfg_clock_24h = !is12hour;
-                        cfg_clock_show_seconds = text.indexOf('s') >= 0
-
-                    }
+                    onTextChanged: onClockFormatChange()
                 }
                 Label {
                     text: Qt.formatDateTime(new Date(), cfg_clock_timeformat)
+                }
+            }
+
+            RowLayout {
+                visible: false
+                Layout.fillWidth: true
+                CheckBox {
+                    id: clock_line_2
+                    checked: false
+                    onCheckedChanged: onClockFormatChange()
+                }
+                Label {
+                    text: i18n("Line 2:")
+                }
+                TextField {
+                    Layout.fillWidth: true
+                    id: clock_timeformat_2
+                    onTextChanged: onClockFormatChange()
+                }
+                Label {
+                    text: Qt.formatDateTime(new Date(), cfg_clock_timeformat_2)
+                }
+                Button {
+                    property string dateFormat: {
+                        // org.kde.plasma.digitalclock
+                        // remove "dddd" from the locale format string
+                        // /all/ locales in LongFormat have "dddd" either
+                        // at the beginning or at the end. so we just
+                        // remove it + the delimiter and space
+                        var format = Qt.locale().dateFormat(Locale.LongFormat);
+                        format = format.replace(/(^dddd.?\s)|(,?\sdddd$)/, "");
+                        return;
+                    }
+                    text: Qt.formatDate(new Date(), dateFormat)
+                    onClicked: cfg_clock_timeformat_2 = dateFormat
+                }
+                Button {
+                    property string dateFormat: Qt.locale().dateFormat(Locale.ShortFormat);
+                    text: Qt.formatDate(new Date(), dateFormat)
+                    onClicked: cfg_clock_timeformat_2 = dateFormat
                 }
             }
 

package/contents/ui/AgendaView.qml
@@ -20,6 +20,8 @@ Item {
     property bool clipPastEvents: false
     property bool clipPastEventsToday: false
     property bool cfg_clock_24h: false
+    property int cfg_agenda_weather_icon_height: 24
+    property bool cfg_agenda_weather_show_text: false
 
     signal newEventFormOpened(variant agendaItem, variant newEventCalendarId)
     signal submitNewEventForm(variant calendarId, variant date, string text)
@@ -33,7 +35,7 @@ Item {
 
     // Testing with qmlview
     Rectangle {
-        visible: !popup
+        visible: typeof popup === 'undefined'
         color: PlasmaCore.ColorScope.backgroundColor
         anchors.fill: parent
     }
@@ -56,51 +58,78 @@ Item {
             anchors.right: parent.right
             spacing: 10
 
-            Column {
-                width: 50
+            LinkRect {
                 Layout.alignment: Qt.AlignTop
 
-                FontIcon {
-                    visible: showWeather
-                    source: weatherIcon
-                    height: 16
-                    width: width
-                    anchors {
-                        left: parent.left
-                        right: parent.right
+                Column {
+                    id: itemWeatherColumn
+                    width: 50
+                    Layout.alignment: Qt.AlignTop
+
+                    FontIcon {
+                        visible: showWeather
+                        source: weatherIcon
+                        height: cfg_agenda_weather_icon_height
+                        anchors {
+                            left: parent.left
+                            right: parent.right
+                        }
+                    }
+
+                    Text {
+                        id: itemWeatherText
+                        visible: showWeather && cfg_agenda_weather_show_text
+                        text: weatherText
+                        color: PlasmaCore.ColorScope.textColor
+                        opacity: 0.5
+                        anchors {
+                            left: parent.left
+                            right: parent.right
+                        }
+                        horizontalAlignment: Text.AlignHCenter
+                    }
+
+                    Text {
+                        id: itemWeatherTemps
+                        visible: showWeather
+                        text: tempHigh + '° | ' + tempLow + '°'
+                        color: PlasmaCore.ColorScope.textColor
+                        opacity: 0.5
+                        anchors {
+                            left: parent.left
+                            right: parent.right
+                        }
+                        horizontalAlignment: Text.AlignHCenter
                     }
                 }
 
-                Text {
-                    id: itemWeatherText
-                    visible: showWeather
-                    text: weatherText
-                    color: PlasmaCore.ColorScope.textColor
-                    opacity: 0.5
-                    anchors {
-                        left: parent.left
-                        right: parent.right
-                    }
-                    horizontalAlignment: Text.AlignHCenter
-                }
+                // PlasmaCore.ToolTipArea {
+                //     anchors.fill: parent
+                //     mainText: weatherDescription
+                //     subText: {
+                //         var lines = [];
+                //         lines.push(tempHigh + '° | ' + tempLow + '°');
+                //         lines.push('<b>Morning:</b> ' + tempMorn + '°');
+                //         lines.push('<b>Day:</b> ' + tempDay + '°');
+                //         lines.push('<b>Evening:</b> ' + tempEve + '°');
+                //         lines.push('<b>Night:</b> ' + tempNight + '°');
+                //         return lines.join('<br>');
+                //     }
+                //     icon: weatherIcon
+                // }
 
-                Text {
-                    id: itemWeatherTemps
-                    visible: showWeather
-                    text: tempHigh + '° | ' + tempLow + '°'
-                    color: PlasmaCore.ColorScope.textColor
-                    opacity: 0.5
-                    anchors {
-                        left: parent.left
-                        right: parent.right
+                onClicked: {
+                    console.log('agendaItem.date.clicked', date)
+                    if (true) {
+                        // cfg_agenda_weather_clicked == "browser_viewcityforecast"
+                        if (config.weather_city_id) {
+                            Shared.openOpenWeatherMapCityUrl(config.weather_city_id);
+                        }
                     }
-                    horizontalAlignment: Text.AlignHCenter
                 }
             }
-            Rectangle {
-                width: itemDateColumn.width
-                height: itemDateColumn.height
-                color: mouseArea.containsMouse ? theme.buttonBackgroundColor : "transparent"
+
+            LinkRect {
                 Layout.alignment: Qt.AlignTop
 
                 Column {
@@ -139,20 +168,14 @@ Item {
                     }
                 }
 
-                MouseArea {
-                    id: mouseArea
-                    anchors.fill: itemDateColumn
-                    hoverEnabled: true
-                    cursorShape: containsMouse ? Qt.PointingHandCursor : Qt.ArrowCursor
-                    onClicked: {
-                        console.log('agendaItem.date.clicked', date)
-                        if (false) {
-                            // cfg_agenda_date_clicked == "browser_newevent"
-                            Shared.openGoogleCalendarNewEventUrl(date);
-                        } else if (true) {
-                            // cfg_agenda_date_clicked == "agenda_newevent"
-                            newEventForm.visible = !newEventForm.visible
-                        }
+                onClicked: {
+                    console.log('agendaItem.date.clicked', date)
+                    if (false) {
+                        // cfg_agenda_date_clicked == "browser_newevent"
+                        Shared.openGoogleCalendarNewEventUrl(date);
+                    } else if (true) {
+                        // cfg_agenda_date_clicked == "agenda_newevent"
+                        newEventForm.visible = !newEventForm.visible
                     }
                 }
             }
@@ -206,74 +229,68 @@ Item {
                     Repeater {
                         model: events
 
-                        delegate: Rectangle {
+                        // delegate: Rectangle {
+                        delegate: LinkRect {
+                            width: undefined
                             Layout.fillWidth: true
                             height: eventColumn.height
-                            color: mouseArea.containsMouse ? theme.buttonBackgroundColor : "transparent"
 
-                            Rectangle {
-                                anchors.left: parent.left
-                                anchors.top: parent.top
-                                anchors.bottom: parent.bottom
-                                anchors.leftMargin: -3 - width
-                                width: 2
-                                color: backgroundColor
-                            }
-
-                            ColumnLayout {
-                                id: eventColumn
-                                // Layout.fillWidth: true
-
-                                Text {
-                                    id: eventSummary
-                                    text: summary
-                                    color: PlasmaCore.ColorScope.textColor
+                            RowLayout {
+                                Rectangle {
+                                    width: 2
+                                    height: eventColumn.height
+                                    color: model.backgroundColor
                                 }
 
-                                Text {
-                                    id: eventDateTime
-                                    text: {
-                                        if (start.date) {
-                                            return "All Day"
-                                        } else {
-                                            var s = formatEventTime(start.dateTime);
-                                            if (start.dateTime.valueOf() != end.dateTime.valueOf()) {
-                                                s += " - ";
-                                                if (!(start.dateTime.getFullYear() == end.dateTime.getFullYear() && start.dateTime.getMonth() == end.dateTime.getMonth() && start.dateTime.getDate() == end.dateTime.getDate())) {
-                                                    s += Qt.formatDateTime(end.dateTime, "MMM d") + ", ";
+                                ColumnLayout {
+                                    id: eventColumn
+                                    // Layout.fillWidth: true
+
+                                    Text {
+                                        id: eventSummary
+                                        text: summary
+                                        color: PlasmaCore.ColorScope.textColor
+                                    }
+
+                                    Text {
+                                        id: eventDateTime
+                                        text: {
+                                            if (start.date) {
+                                                return "All Day"
+                                            } else {
+                                                var s = formatEventTime(start.dateTime);
+                                                if (start.dateTime.valueOf() != end.dateTime.valueOf()) {
+                                                    s += " - ";
+                                                    if (!(start.dateTime.getFullYear() == end.dateTime.getFullYear() && start.dateTime.getMonth() == end.dateTime.getMonth() && start.dateTime.getDate() == end.dateTime.getDate())) {
+                                                        s += Qt.formatDateTime(end.dateTime, "MMM d") + ", ";
+                                                    }
+                                                    s += formatEventTime(end.dateTime);
                                                 }
-                                                s += formatEventTime(end.dateTime);
+                                                return s;
                                             }
-                                            return s;
                                         }
-                                    }
-                                    color: PlasmaCore.ColorScope.textColor
-                                    opacity: 0.75
+                                        color: PlasmaCore.ColorScope.textColor
+                                        opacity: 0.75
 
-                                    function formatEventTime(dateTime) {
-                                        var timeFormat = "h"
-                                        if (dateTime.getMinutes() != 0) {
-                                            timeFormat += ":mm"
+                                        function formatEventTime(dateTime) {
+                                            var timeFormat = "h"
+                                            if (dateTime.getMinutes() != 0) {
+                                                timeFormat += ":mm"
+                                            }
+                                            if (!cfg_clock_24h) {
+                                                timeFormat += " AP"
+                                            }
+                                            return Qt.formatDateTime(dateTime, timeFormat)
                                         }
-                                        if (!cfg_clock_24h) {
-                                            timeFormat += " AP"
-                                        }
-                                        return Qt.formatDateTime(dateTime, timeFormat)
                                     }
                                 }
                             }
 
-                            MouseArea {
-                                id: mouseArea
-                                anchors.fill: parent
-                                hoverEnabled: true
-                                cursorShape: containsMouse ? Qt.PointingHandCursor : Qt.ArrowCursor
-                                onClicked: {
-                                    console.log('agendaItem.event.clicked', start.date)
-                                    if (true) {
-                                        // cfg_agenda_event_clicked == "browser_viewevent"
-                                        Qt.openUrlExternally(htmlLink)
-                                    }
+                            onClicked: {
+                                console.log('agendaItem.event.clicked', start.date)
+                                if (true) {
+                                    // cfg_agenda_event_clicked == "browser_viewevent"
+                                    Qt.openUrlExternally(htmlLink)
                                 }
                             }
                         }
@@ -306,6 +323,33 @@ Item {
         }
         // If the date is greater than any item in the agenda, scroll to the bottom.
         agendaListView.positionViewAtEnd()
+    }
+
+    function buildAgendaItem(dateTime) {
+        return {
+            date: dateTime,
+            events: [],
+            showWeather: false,
+            tempLow: 0,
+            tempHigh: 0,
+            weatherIcon: "",
+            weatherText: "",
+            weatherDescription: "",
+            weather: {
+                temp: {
+                    morn: 0,
+                    day: 0,
+                    eve: 0,
+                    night: 0,
+                },
+                pressure: 0,
+                humidity: 0,
+                speed: 0,
+                clouds: 93,
+                rain: 0,
+                snow: 2.69,
+            },
+        };
     }
 
     function parseGCalEvents(data) {
@@ -353,15 +397,7 @@ Item {
                 if (agendaItem) {
                     agendaItemList.push(agendaItem);
                 }
-                agendaItem = {
-                    date: eventItem.start.dateTime,
-                    events: [],
-                    showWeather: false,
-                    tempLow: 0,
-                    tempHigh: 0,
-                    weatherIcon: "",
-                    weatherText: "",
-                };
+                agendaItem = buildAgendaItem(eventItem.start.dateTime);
             }
 
             agendaItem.events.push(eventItem);
@@ -391,15 +427,7 @@ Item {
                 }
 
                 // It doesn't, so we need to insert an item.
-                var newAgendaItem = {
-                    date: new Date(day),
-                    events: [],
-                    showWeather: false,
-                    tempLow: 0,
-                    tempHigh: 0,
-                    weatherIcon: "",
-                    weatherText: "",
-                };
+                var newAgendaItem = buildAgendaItem(new Date(day));
 
                 // Insert before the agendaItem with a higher date.
                 for (var i = 0; i < agendaItemList.length; i++) {
@@ -483,7 +511,13 @@ Item {
                     var weatherIcon = weatherIconMap[forecastItem.weather[0].icon] || 'weather-severe-alert';
                     agendaModel.setProperty(i, 'weatherIcon', weatherIcon);
                     agendaModel.setProperty(i, 'weatherText', forecastItem.weather[0].main);
+                    agendaModel.setProperty(i, 'weatherDescription', forecastItem.weather[0].description);
+                    // agendaModel.setProperty(i, 'tempMorn', Math.round(forecastItem.temp.morn));
+                    // agendaModel.setProperty(i, 'tempDay', Math.round(forecastItem.temp.day));
+                    // agendaModel.setProperty(i, 'tempEve', Math.round(forecastItem.temp.eve));
+                    // agendaModel.setProperty(i, 'tempNight', Math.round(forecastItem.temp.night));
                     agendaModel.setProperty(i, 'showWeather', true);
+                    
                     break;
                 }
             }
@@ -494,9 +528,9 @@ Item {
         parseGCalEvents({ "items": [], });
         parseWeatherForecast({ "list": [], });
 
-        if (typeof main === 'undefined') {
+        if (typeof root === 'undefined') {
             clipPastEvents = false
-            var debugData = {
+            var debugEventData = {
                 "items": [
                     {
                         "kind": "calendar#event",
@@ -529,8 +563,52 @@ Item {
                     },
                 ] 
             };
-            debugData.items.push(debugData.items[0]);
-            parseGCalEvents(debugData);
+            debugEventData.items.push(debugEventData.items[0]);
+            parseGCalEvents(debugEventData);
+
+            var debugWeatherData = {
+                "city": {
+                    "id": 1,
+                    "name": "Area 51",
+                    "coord": {
+                        "lon": 0.249672,
+                        "lat": 0.550098
+                    },
+                    "country": "CA",
+                    "population": 0
+                },
+                "cod": "200",
+                "message": 0.0275,
+                "cnt": 7,
+                "list": [
+                    {
+                        "dt": 1459270800,
+                        "temp": {
+                            "day": 5.3,
+                            "min": -6.14,
+                            "max": 5.43,
+                            "night": -6.14,
+                            "eve": 1.01,
+                            "morn": 5.3
+                        },
+                        "pressure": 1006.93,
+                        "humidity": 49,
+                        "weather": [
+                            {
+                            "id": 800,
+                            "main": "Clear",
+                            "description": "clear sky",
+                            "icon": "01d"
+                            }
+                        ],
+                        "speed": 6.82,
+                        "deg": 327,
+                        "clouds": 0
+                    },
+                ],
+            };
+            parseWeatherForecast(debugWeatherData);
+
         }
     }
 }

package/contents/ui/ClockView.qml
@@ -28,7 +28,15 @@ Item {
 
     width: labels.width
     Layout.minimumWidth: labels.width
-    Layout.maximumWidth: labels.width
+    // Layout.maximumWidth: timeLabel.width
+
+    property date currentTime: {
+        if (typeof dataSource === 'undefined') {
+            return new Date();
+        } else {
+            return dataSource.data["Local"]["DateTime"];
+        }
+    }
 
     property string cfg_clock_timeformat: "h:mm AP"
     property bool cfg_clock_24h: false
@@ -36,17 +44,9 @@ Item {
     
     // Testing with qmlview
     Rectangle {
-        visible: !root
+        visible: typeof root === 'undefined'
         color: PlasmaCore.ColorScope.backgroundColor
         anchors.fill: parent
-    }
-
-    MouseArea {
-        id: mouseArea
-
-        anchors.fill: parent
-
-        onClicked: plasmoid.expanded = !plasmoid.expanded
     }
 
     Row {
@@ -55,7 +55,7 @@ Item {
 
         Components.Label {
             id: timerLabel
-            visible: timerView && timerView.timerSeconds > 0
+            visible: false
 
             font.family: theme.defaultFont.family
             font.pointSize: 1024
@@ -75,7 +75,7 @@ Item {
             // anchors.horizontalCenter: clock.horizontalCenter
 
             text: {
-                return timerView ? "T" + timerView.timerSeconds : "0:00"
+                return "0:00"
             }
         }
 
@@ -83,7 +83,7 @@ Item {
             source: "chronometer"
             width: sizehelper.height
             height: sizehelper.height
-            visible: timerView
+            visible: false
         }
 
         Components.Label {
@@ -108,9 +108,9 @@ Item {
 
             text: {
                 if (clock.cfg_clock_timeformat) {
-                    return Qt.formatDateTime(dataSource.data["Local"]["DateTime"], clock.cfg_clock_timeformat);
+                    return Qt.formatDateTime(clock.currentTime, clock.cfg_clock_timeformat);
                 } else {
-                    return Qt.formatTime(dataSource.data["Local"]["DateTime"], clock.cfg_clock_24h ? "hh:mm" : "h:mm AP");
+                    return Qt.formatTime(clock.currentTime, clock.cfg_clock_24h ? "hh:mm" : "h:mm AP");
                 }
             }
         }
@@ -127,7 +127,7 @@ Item {
 
         font.weight: timeLabel.font.weight
         font.italic: timeLabel.font.italic
-        font.pixelSize: 1024
+        // font.pixelSize: 1024
         font.pointSize: 1024
         verticalAlignment: Text.AlignVCenter
         visible: false

package/contents/ui/FontIcon.qml
@@ -21,6 +21,7 @@ Item {
 
         font.family: "weathericons"
         font.pointSize: -1
+        font.pixelSize: parent.height
         anchors.centerIn: parent
     }
 

package/contents/ui/LinkRect.qml
@@ -1,0 +1,23 @@
+import QtQuick 2.0
+import QtQuick.Layouts 1.0
+import org.kde.plasma.core 2.0 as PlasmaCore
+
+Rectangle {
+    width: childrenRect.width
+    height: childrenRect.height
+    property color backgroundColor: "transparent"
+    property color backgroundHoverColor: theme.buttonBackgroundColor
+    color: mouseArea.containsMouse ? backgroundHoverColor : backgroundColor
+
+    signal clicked()
+
+    MouseArea {
+        id: mouseArea
+        anchors.fill: parent
+        hoverEnabled: true
+        cursorShape: containsMouse ? Qt.PointingHandCursor : Qt.ArrowCursor
+        onClicked: {
+            parent.clicked()
+        }
+    }
+}

package/contents/ui/TimerView.qml
@@ -21,7 +21,7 @@ Item {
 
     // Testing with qmlview
     Rectangle {
-        visible: !popup
+        visible: typeof popup === 'undefined'
         color: PlasmaCore.ColorScope.backgroundColor
         anchors.fill: parent
     }

package/contents/ui/config/ConfigAgenda.qml
@@ -17,6 +17,55 @@ ColumnLayout {
     ColumnLayout {
         Layout.alignment: Qt.AlignTop | Qt.AlignLeft
 
+        GroupBox {
+            Layout.fillWidth: true
+
+            ColumnLayout {
+
+                RowLayout {
+                    Label {
+                        text: "Click Weather:"
+                        Layout.alignment: Qt.AlignTop | Qt.AlignLeft
+                    }
+                    ColumnLayout {
+                        ExclusiveGroup { id: agenda_weather_clickGroup }
+                        RadioButton {
+                            text: "Open City Forecast In Browser"
+                            exclusiveGroup: agenda_weather_clickGroup
+                            checked: true
+                        }
+                    }
+                }
+
+                RowLayout {
+                    CheckBox {
+                        enabled: false
+                        checked: true
+                        text: "Weather Icon"
+                    }
+                    Slider {
+                        id: agenda_weather_icon_size
+                        enabled: false
+                        minimumValue: 12
+                        maximumValue: 48
+                        stepSize: 1
+                        value: 24
+                    }
+                    Label {
+                        text: agenda_weather_icon_size.value + 'px'
+                    }
+
+                }
+
+                RowLayout {
+                    CheckBox {
+                        enabled: false
+                        checked: false
+                        text: "Weather Text"
+                    }
+                }
+            }
+        }
 
         GroupBox {
             Layout.fillWidth: true

commit 31f2c9d09366d5826d3c02199dd5026da9177634

Fix a date preset button displaying nothing.
Error introduced in 3646cf5c when the button was added.
Previously, Qt.formatDate(now, undefined) returned something,
but it currently returns an empty string.

package/contents/ui/config/ConfigGeneral.qml
@@ -322,7 +322,7 @@ ConfigPage {
                         // remove it + the delimiter and space
                         var format = Qt.locale().dateFormat(Locale.LongFormat);
                         format = format.replace(/(^dddd.?\s)|(,?\sdddd$)/, "");
-                        return;
+                        return format; 
                     }
                     text: Qt.formatDate(new Date(), dateFormat)
                     onClicked: cfg_clock_timeformat_2 = dateFormat