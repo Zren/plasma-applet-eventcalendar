1d1ffebe6a16c869694def1fc652e607378aebca
Chris Holland <zrenfire@gmail.com>
2019-01-23 01:02:21 -0500

    tabify + remove semicolons

--- package/contents/code/ColorIdMap.js
+++ package/contents/code/ColorIdMap.js
@@ -1,148 +1,148 @@
 .pragma library
 
 var colorIdMap = {
- "calendar": {
-  "1": {
-   "background": "#ac725e",
-   "foreground": "#1d1d1d"
-  },
-  "2": {
-   "background": "#d06b64",
-   "foreground": "#1d1d1d"
-  },
-  "3": {
-   "background": "#f83a22",
-   "foreground": "#1d1d1d"
-  },
-  "4": {
-   "background": "#fa573c",
-   "foreground": "#1d1d1d"
-  },
-  "5": {
-   "background": "#ff7537",
-   "foreground": "#1d1d1d"
-  },
-  "6": {
-   "background": "#ffad46",
-   "foreground": "#1d1d1d"
-  },
-  "7": {
-   "background": "#42d692",
-   "foreground": "#1d1d1d"
-  },
-  "8": {
-   "background": "#16a765",
-   "foreground": "#1d1d1d"
-  },
-  "9": {
-   "background": "#7bd148",
-   "foreground": "#1d1d1d"
-  },
-  "10": {
-   "background": "#b3dc6c",
-   "foreground": "#1d1d1d"
-  },
-  "11": {
-   "background": "#fbe983",
-   "foreground": "#1d1d1d"
-  },
-  "12": {
-   "background": "#fad165",
-   "foreground": "#1d1d1d"
-  },
-  "13": {
-   "background": "#92e1c0",
-   "foreground": "#1d1d1d"
-  },
-  "14": {
-   "background": "#9fe1e7",
-   "foreground": "#1d1d1d"
-  },
-  "15": {
-   "background": "#9fc6e7",
-   "foreground": "#1d1d1d"
-  },
-  "16": {
-   "background": "#4986e7",
-   "foreground": "#1d1d1d"
-  },
-  "17": {
-   "background": "#9a9cff",
-   "foreground": "#1d1d1d"
-  },
-  "18": {
-   "background": "#b99aff",
-   "foreground": "#1d1d1d"
-  },
-  "19": {
-   "background": "#c2c2c2",
-   "foreground": "#1d1d1d"
-  },
-  "20": {
-   "background": "#cabdbf",
-   "foreground": "#1d1d1d"
-  },
-  "21": {
-   "background": "#cca6ac",
-   "foreground": "#1d1d1d"
-  },
-  "22": {
-   "background": "#f691b2",
-   "foreground": "#1d1d1d"
-  },
-  "23": {
-   "background": "#cd74e6",
-   "foreground": "#1d1d1d"
-  },
-  "24": {
-   "background": "#a47ae2",
-   "foreground": "#1d1d1d"
-  }
- },
- "event": {
-  "1": {
-   "background": "#a4bdfc",
-   "foreground": "#1d1d1d"
-  },
-  "2": {
-   "background": "#7ae7bf",
-   "foreground": "#1d1d1d"
-  },
-  "3": {
-   "background": "#dbadff",
-   "foreground": "#1d1d1d"
-  },
-  "4": {
-   "background": "#ff887c",
-   "foreground": "#1d1d1d"
-  },
-  "5": {
-   "background": "#fbd75b",
-   "foreground": "#1d1d1d"
-  },
-  "6": {
-   "background": "#ffb878",
-   "foreground": "#1d1d1d"
-  },
-  "7": {
-   "background": "#46d6db",
-   "foreground": "#1d1d1d"
-  },
-  "8": {
-   "background": "#e1e1e1",
-   "foreground": "#1d1d1d"
-  },
-  "9": {
-   "background": "#5484ed",
-   "foreground": "#1d1d1d"
-  },
-  "10": {
-   "background": "#51b749",
-   "foreground": "#1d1d1d"
-  },
-  "11": {
-   "background": "#dc2127",
-   "foreground": "#1d1d1d"
-  }
- }
+	"calendar": {
+		"1": {
+			"background": "#ac725e",
+			"foreground": "#1d1d1d"
+		},
+		"2": {
+			"background": "#d06b64",
+			"foreground": "#1d1d1d"
+		},
+		"3": {
+			"background": "#f83a22",
+			"foreground": "#1d1d1d"
+		},
+		"4": {
+			"background": "#fa573c",
+			"foreground": "#1d1d1d"
+		},
+		"5": {
+			"background": "#ff7537",
+			"foreground": "#1d1d1d"
+		},
+		"6": {
+			"background": "#ffad46",
+			"foreground": "#1d1d1d"
+		},
+		"7": {
+			"background": "#42d692",
+			"foreground": "#1d1d1d"
+		},
+		"8": {
+			"background": "#16a765",
+			"foreground": "#1d1d1d"
+		},
+		"9": {
+			"background": "#7bd148",
+			"foreground": "#1d1d1d"
+		},
+		"10": {
+			"background": "#b3dc6c",
+			"foreground": "#1d1d1d"
+		},
+		"11": {
+			"background": "#fbe983",
+			"foreground": "#1d1d1d"
+		},
+		"12": {
+			"background": "#fad165",
+			"foreground": "#1d1d1d"
+		},
+		"13": {
+			"background": "#92e1c0",
+			"foreground": "#1d1d1d"
+		},
+		"14": {
+			"background": "#9fe1e7",
+			"foreground": "#1d1d1d"
+		},
+		"15": {
+			"background": "#9fc6e7",
+			"foreground": "#1d1d1d"
+		},
+		"16": {
+			"background": "#4986e7",
+			"foreground": "#1d1d1d"
+		},
+		"17": {
+			"background": "#9a9cff",
+			"foreground": "#1d1d1d"
+		},
+		"18": {
+			"background": "#b99aff",
+			"foreground": "#1d1d1d"
+		},
+		"19": {
+			"background": "#c2c2c2",
+			"foreground": "#1d1d1d"
+		},
+		"20": {
+			"background": "#cabdbf",
+			"foreground": "#1d1d1d"
+		},
+		"21": {
+			"background": "#cca6ac",
+			"foreground": "#1d1d1d"
+		},
+		"22": {
+			"background": "#f691b2",
+			"foreground": "#1d1d1d"
+		},
+		"23": {
+			"background": "#cd74e6",
+			"foreground": "#1d1d1d"
+		},
+		"24": {
+			"background": "#a47ae2",
+			"foreground": "#1d1d1d"
+		}
+	},
+	"event": {
+		"1": {
+			"background": "#a4bdfc",
+			"foreground": "#1d1d1d"
+		},
+		"2": {
+			"background": "#7ae7bf",
+			"foreground": "#1d1d1d"
+		},
+		"3": {
+			"background": "#dbadff",
+			"foreground": "#1d1d1d"
+		},
+		"4": {
+			"background": "#ff887c",
+			"foreground": "#1d1d1d"
+		},
+		"5": {
+			"background": "#fbd75b",
+			"foreground": "#1d1d1d"
+		},
+		"6": {
+			"background": "#ffb878",
+			"foreground": "#1d1d1d"
+		},
+		"7": {
+			"background": "#46d6db",
+			"foreground": "#1d1d1d"
+		},
+		"8": {
+			"background": "#e1e1e1",
+			"foreground": "#1d1d1d"
+		},
+		"9": {
+			"background": "#5484ed",
+			"foreground": "#1d1d1d"
+		},
+		"10": {
+			"background": "#51b749",
+			"foreground": "#1d1d1d"
+		},
+		"11": {
+			"background": "#dc2127",
+			"foreground": "#1d1d1d"
+		},
+	},
 }
--- package/contents/code/DebugFixtures.js
+++ package/contents/code/DebugFixtures.js
@@ -1,164 +1,164 @@
 .pragma library
 
 function getCalendar() {
-    return {
-        "id": "debug",
-        "backgroundColor": "#9a9cff",
-        "accessRole": "owner",
-    }
+	return {
+		"id": "debug",
+		"backgroundColor": "#9a9cff",
+		"accessRole": "owner",
+	}
 }
 
 function getEventData() {
-    var debugEventData = {
-        "items": []
-    };
-    function addEvent(summary, start, end, description) {
-        var newEvent = {
-            "kind": "calendar#event",
-            "etag": "\"2561779720126000\"",
-            "id": "debug_" + start.dateTime.getTime() + "_" + end.dateTime.getTime(),
-            "status": "confirmed",
-            "htmlLink": "https://www.google.com/calendar/event?eid=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&ctz=Etc/UTC",
-            "created": "2008-03-24T22:34:26.000Z",
-            "updated": "2010-08-04T02:44:20.063Z",
-            "summary": summary,
-            "start": start,
-            "end": end,
-            // "recurringEventId": "a1a1a1a1a1a1a1a1a1a1a1a1a1",
-            "originalStartTime": {
-                "date": "2016-03-25"
-            },
-            "transparency": "transparent",
-            "iCalUID": "a1a1a1a1a1a1a1a1a1a1a1a1a1@google.com",
-            "sequence": 0,
-            "reminders": {
-                "useDefault": false
-            },
-        }
-        if (typeof description !== "undefined") {
-            newEvent.description = description
-        }
-        debugEventData.items.push(newEvent)
-        return newEvent
-    }
+	var debugEventData = {
+		"items": []
+	}
+	function addEvent(summary, start, end, description) {
+		var newEvent = {
+			"kind": "calendar#event",
+			"etag": "\"2561779720126000\"",
+			"id": "debug_" + start.dateTime.getTime() + "_" + end.dateTime.getTime(),
+			"status": "confirmed",
+			"htmlLink": "https://www.google.com/calendar/event?eid=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&ctz=Etc/UTC",
+			"created": "2008-03-24T22:34:26.000Z",
+			"updated": "2010-08-04T02:44:20.063Z",
+			"summary": summary,
+			"start": start,
+			"end": end,
+			// "recurringEventId": "a1a1a1a1a1a1a1a1a1a1a1a1a1",
+			"originalStartTime": {
+				"date": "2016-03-25"
+			},
+			"transparency": "transparent",
+			"iCalUID": "a1a1a1a1a1a1a1a1a1a1a1a1a1@google.com",
+			"sequence": 0,
+			"reminders": {
+				"useDefault": false
+			},
+		}
+		if (typeof description !== "undefined") {
+			newEvent.description = description
+		}
+		debugEventData.items.push(newEvent)
+		return newEvent
+	}
 
-    function dateString(d) {
-        return d.toISOString().substr(0, 10)
-    }
-    function nowPlus(n) {
-        var now = new Date()
-        var d = new Date(now.getFullYear(), now.getMonth(), now.getDate() + n, 0, 0, 0)
-        return d
-    }
+	function dateString(d) {
+		return d.toISOString().substr(0, 10)
+	}
+	function nowPlus(n) {
+		var now = new Date()
+		var d = new Date(now.getFullYear(), now.getMonth(), now.getDate() + n, 0, 0, 0)
+		return d
+	}
 
-    function addAllDayTestEvent(summary, startDaysFromNow, endDaysFromNow) {
-        var start = nowPlus(startDaysFromNow)
-        var end = nowPlus(endDaysFromNow)
-        return addEvent(summary, {
-            date: dateString(start),
-            dateTime: start,
-        }, {
-            date: dateString(end),
-            dateTime: end,
-        })
-    }
-    function addMinuteTestEvent(summary, startMinutesFromNow) {
-        var start = new Date()
-        start = new Date(start.getFullYear(), start.getMonth(), start.getDate(), start.getHours(), start.getMinutes(), 0);
-        start.setMinutes(start.getMinutes() + startMinutesFromNow)
-        var end = new Date(start)
-        end.setMinutes(end.getMinutes() + 1)
-        return addEvent(summary, {dateTime: start }, {dateTime: end })
-    }
+	function addAllDayTestEvent(summary, startDaysFromNow, endDaysFromNow) {
+		var start = nowPlus(startDaysFromNow)
+		var end = nowPlus(endDaysFromNow)
+		return addEvent(summary, {
+			date: dateString(start),
+			dateTime: start,
+		}, {
+			date: dateString(end),
+			dateTime: end,
+		})
+	}
+	function addMinuteTestEvent(summary, startMinutesFromNow) {
+		var start = new Date()
+		start = new Date(start.getFullYear(), start.getMonth(), start.getDate(), start.getHours(), start.getMinutes(), 0)
+		start.setMinutes(start.getMinutes() + startMinutesFromNow)
+		var end = new Date(start)
+		end.setMinutes(end.getMinutes() + 1)
+		return addEvent(summary, {dateTime: start }, {dateTime: end })
+	}
 
-    addAllDayTestEvent("Dude's Birthday", 0, 1);
-    addAllDayTestEvent("Dudette's Birthday", 1, 2);
-    addAllDayTestEvent("Multiday Event", 3, 7);
-    for (var i = 0; i < 5; i++) {
-        addMinuteTestEvent("Minute Event " + i, i);
-    }
+	addAllDayTestEvent("Dude's Birthday", 0, 1)
+	addAllDayTestEvent("Dudette's Birthday", 1, 2)
+	addAllDayTestEvent("Multiday Event", 3, 7)
+	for (var i = 0; i < 5; i++) {
+		addMinuteTestEvent("Minute Event " + i, i)
+	}
 
-    // This test is known to crash plasmashell.
-    // See https://github.com/Zren/plasma-applets/issues/60
-    // Note: Emoji's do not show up in plasmoidviewer.
-    addAllDayTestEvent("Emoji Test 1: '🤑'", 0, 1); // Pasted
-    addAllDayTestEvent("Emoji Test 2: '\uD83E\uDD11'", 0, 1); // UTF-16 (hex)
+	// This test is known to crash plasmashell.
+	// See https://github.com/Zren/plasma-applets/issues/60
+	// Note: Emoji's do not show up in plasmoidviewer.
+	addAllDayTestEvent("Emoji Test 1: '🤑'", 0, 1) // Pasted
+	addAllDayTestEvent("Emoji Test 2: '\uD83E\uDD11'", 0, 1) // UTF-16 (hex)
 
-    // Unicode / Foreign Characters
-    addAllDayTestEvent("Unicode Test 1: 'ひらがな'", 0, 1); // Japenese (Hiragana)
+	// Unicode / Foreign Characters
+	addAllDayTestEvent("Unicode Test 1: 'ひらがな'", 0, 1) // Japenese (Hiragana)
 
-    // Long summary
-    addAllDayTestEvent("One Two Three Four Five Six Seven Eight Nine Ten One Two Three Four Five Six Seven Eight Nine Ten One Two Three Four Five Six Seven Eight Nine Ten", 1, 2);
-    addAllDayTestEvent("OneTwoThreeFourFiveSixSevenEightNineTenOneTwoThreeFourFiveSixSevenEightNineTen", -2, -1);
-    addAllDayTestEvent("OneTwoThreeFourFiveSixSevenEightNineTenOneTwoThreeFourFiveSixSevenEightNineTen", -1, 0);
+	// Long summary
+	addAllDayTestEvent("One Two Three Four Five Six Seven Eight Nine Ten One Two Three Four Five Six Seven Eight Nine Ten One Two Three Four Five Six Seven Eight Nine Ten", 1, 2)
+	addAllDayTestEvent("OneTwoThreeFourFiveSixSevenEightNineTenOneTwoThreeFourFiveSixSevenEightNineTen", -2, -1)
+	addAllDayTestEvent("OneTwoThreeFourFiveSixSevenEightNineTenOneTwoThreeFourFiveSixSevenEightNineTen", -1, 0)
 
 
-    // Description
-    var event = addAllDayTestEvent("Description Test", 4, 5)
-    event.description = "Short description."
+	// Description
+	var event = addAllDayTestEvent("Description Test", 4, 5)
+	event.description = "Short description."
 
-    // Long description
-    event = addAllDayTestEvent("Long RichText Description Test", 4, 5)
-    event.description = "Lorem <b>ipsum</b> dolor sit amet, consectetur <u>adipiscing</u> elit, sed do eiusmod tempor incididunt ut labore et dolore magna <a href=\"https://www.google.com\">aliqua</a>. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br><br>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><ul><li>Apples<br></li><li>Oranges<br></li><li>Carrots<br></li></ul>"
+	// Long description
+	event = addAllDayTestEvent("Long RichText Description Test", 4, 5)
+	event.description = "Lorem <b>ipsum</b> dolor sit amet, consectetur <u>adipiscing</u> elit, sed do eiusmod tempor incididunt ut labore et dolore magna <a href=\"https://www.google.com\">aliqua</a>. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br><br>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><ul><li>Apples<br></li><li>Oranges<br></li><li>Carrots<br></li></ul>"
 
-    // Hangouts Link
-    var testHangoutLink = "https://hangouts.google.com/hangouts/_/calendar/abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnop38.abcdefghijklmnopqrstuvwx26"
-    
-    event = addAllDayTestEvent("Hangout Link Test", 5, 6)
-    event.hangoutLink = testHangoutLink
+	// Hangouts Link
+	var testHangoutLink = "https://hangouts.google.com/hangouts/_/calendar/abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnop38.abcdefghijklmnopqrstuvwx26"
+	
+	event = addAllDayTestEvent("Hangout Link Test", 5, 6)
+	event.hangoutLink = testHangoutLink
 
-    var event = addAllDayTestEvent("Description + Hangout Test", 5, 6)
-    event.description = "Short description."
-    event.hangoutLink = testHangoutLink
+	var event = addAllDayTestEvent("Description + Hangout Test", 5, 6)
+	event.description = "Short description."
+	event.hangoutLink = testHangoutLink
 
-    for (var i = -30; i <= 30; i++) {
-        addAllDayTestEvent("Day " + i, i, i+1);
-    }
-    
-    return debugEventData;
+	for (var i = -30; i <= 30; i++) {
+		addAllDayTestEvent("Day " + i, i, i+1)
+	}
+	
+	return debugEventData
 }
 
 function getDailyWeatherData() {
-    var debugWeatherData = {
-        "city": {
-            "id": 1,
-            "name": "Area 51",
-            "coord": {
-                "lon": 0.249672,
-                "lat": 0.550098
-            },
-            "country": "CA",
-            "population": 0
-        },
-        "cod": "200",
-        "message": 0.0275,
-        "cnt": 7,
-        "list": [
-            {
-                "dt": Date.now()/1000,
-                "temp": {
-                    "day": 5.3,
-                    "min": -6.14,
-                    "max": 5.43,
-                    "night": -6.14,
-                    "eve": 1.01,
-                    "morn": 5.3
-                },
-                "pressure": 1006.93,
-                "humidity": 49,
-                "weather": [
-                    {
-                    "id": 800,
-                    "main": "Clear",
-                    "description": "clear sky",
-                    "icon": "01d"
-                    }
-                ],
-                "speed": 6.82,
-                "deg": 327,
-                "clouds": 0
-            },
-        ],
-    };
-    return debugWeatherData;
+	var debugWeatherData = {
+		"city": {
+			"id": 1,
+			"name": "Area 51",
+			"coord": {
+				"lon": 0.249672,
+				"lat": 0.550098
+			},
+			"country": "CA",
+			"population": 0
+		},
+		"cod": "200",
+		"message": 0.0275,
+		"cnt": 7,
+		"list": [
+			{
+				"dt": Date.now()/1000,
+				"temp": {
+					"day": 5.3,
+					"min": -6.14,
+					"max": 5.43,
+					"night": -6.14,
+					"eve": 1.01,
+					"morn": 5.3
+				},
+				"pressure": 1006.93,
+				"humidity": 49,
+				"weather": [
+					{
+					"id": 800,
+					"main": "Clear",
+					"description": "clear sky",
+					"icon": "01d"
+					}
+				],
+				"speed": 6.82,
+				"deg": 327,
+				"clouds": 0
+			},
+		],
+	}
+	return debugWeatherData
 }
