02bf0b51f3e7b7d69bf2dfcb3f488ef47ac37bea
Chris Holland <zrenfire@gmail.com>
2020-06-27 14:43:33 -0400

    Refactor notifications.py to use a subset of pycanberra (Issue #128)
    Since I can't use the bash command, I need to communicate directly
    with libcanberra. libcanberra is installed with plasma-pa so it should
    always be installed.
    
    This greatly simplifies Dave Barry's pycanberra library.
    https://github.com/totdb/pycanberra/blob/master/pycanberra.py
    
    I still need to wait for playFile to end, and implement loop.

--- package/contents/scripts/notification.py
+++ package/contents/scripts/notification.py
@@ -2,6 +2,8 @@
 import os, sys
 import argparse
 import subprocess
+from enum import Enum, IntEnum
+from ctypes import *
 
 import gi
 gi.require_version('GLib', '2.0')
@@ -10,28 +12,113 @@ from gi.repository import GLib, Notify
 
 
 
+#---
+# Recreate canberra-gtk-play in Python
+#   https://git.0pointer.net/libcanberra.git/tree/src/canberra-gtk-play.c
+# Based on Dave Barry's <dave@psax.org> pycanberra under LGPL 2.1
+#   https://github.com/totdb/pycanberra/blob/master/pycanberra.py
+libcanberra = None
+try:
+	libcanberra = CDLL("libcanberra.so.0")
+except:
+	sys.stderr.write('libcanberra not found\n')
+
+def convertArgs(args):
+	return (
+		arg.encode("latin-1") if isinstance(arg, str) else arg
+		for arg in args
+	)
+
+ca_context = c_void_p
+
+class Canberra:
+	@staticmethod
+	def installed():
+		return libcanberra is not None
+
+	class Code(IntEnum):
+		SUCCESS = 0
+		ERROR_NOTSUPPORTED = -1
+		ERROR_INVALID = -2
+		ERROR_STATE = -3
+		ERROR_OOM = -4
+		ERROR_NODRIVER = -5
+		ERROR_SYSTEM = -6
+		ERROR_CORRUPT = -7
+		ERROR_TOOBIG = -8
+		ERROR_NOTFOUND = -9
+		ERROR_DESTROYED = -10
+		ERROR_CANCELED = -11
+		ERROR_NOTAVAILABLE = -12
+		ERROR_ACCESS = -13
+		ERROR_IO = -14
+		ERROR_INTERNAL = -15
+		ERROR_DISABLED = -16
+		ERROR_FORKED = -17
+		ERROR_DISCONNECTED = -18
+		ERROR_MAX = -19
+
+	class Prop(bytes, Enum):
+		EVENT_ID = b'event.id'
+		EVENT_DESCRIPTION = b'event.description'
+		MEDIA_FILENAME = b'media.filename'
+		APPLICATION_NAME = b'application.name'
+
+	def __init__(self):
+		self.context = ca_context()
+		libcanberra.ca_context_create(byref(self.context))
+
+	def play(self, *props):
+		playId = 0
+		res = libcanberra.ca_context_play(
+			self.context,
+			playId,
+			*convertArgs(props),
+			None, # Must end with NULL to mark end of props
+		)
+		if res != Canberra.Code.SUCCESS:
+			raise Exception(Canberra.Code(res), "Error playing", props)
+
+	def playEvent(self, eventId, *props):
+		props += (Canberra.Prop.EVENT_ID, eventId)
+		self.play(*props)
+
+	def playFile(self, filename, *props):
+		props += (Canberra.Prop.MEDIA_FILENAME, filename)
+		self.play(*props)
+
+
+#---
 # Plasma's Notification server doesn't support sounds,
 # the KNotify manually plays sounds instead. So we manually
 # play them with libcanberra. We can't use canberra-gtk-play since
 # it requires the gnome-session-canberra package in Ubuntu,
 # which is not installed by default.
 def playSound(args):
-	sfxCommand = [
-		"canberra-gtk-play",
-		"--description", args.appName,
+	if not Canberra.installed():
+		sys.stderr.write('skipping playing sound\n')
+		return
+
+	canberra = Canberra()
+	props = [
+		Canberra.Prop.EVENT_DESCRIPTION, args.appName,
+		Canberra.Prop.APPLICATION_NAME, args.appName,
 	]
 
 	if args.sound.startswith('/'):
-		sfxCommand += ["--file", args.sound]
+		canberra.playFile(args.sound, *props)
 	else:
-		sfxCommand += ["--id", args.sound]
+		canberra.playEvent(args.sound, *props)
+
 
 	if args.loop:
-		sfxCommand += ["--loop", args.loop]
+		for i in range(args.loop):
+			# TODO: wait for playEffect to end.
+			# TODO: wrap playEffect in a function, and call it here.
+			pass
 
-	sfxProc = subprocess.Popen(sfxCommand)
 
-
+#---
 def notify(args):
 	sfxProc = None
 
